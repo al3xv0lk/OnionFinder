6b06ec8c62e8f3a75e9857fd62487f37523d085d
al3xv0lk <59628368+al3xv0lk@users.noreply.github.com>
2022-10-27 15:44:39 -0300

    Add files of refactor

---
+++ Helpers/ConsoleHelper.cs
@@ -0,0 +1,12 @@
+namespace OnionFinder.Helpers;
+
+public static class ConsoleHelper
+{ 
+    public static void WhiteSpace(int n = 1)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Helpers/HtmlDocumentHelper.cs
@@ -0,0 +1,36 @@
+using HtmlAgilityPack;
+
+namespace OnionFinder.Helpers;
+
+public static class HtmlDocumentHelper
+{
+  public static List<string> FromAhmia(this HtmlDocument htmlDoc)
+  {
+    try
+    {
+      var tempUrls = new List<string>();
+      var links = htmlDoc.DocumentNode.SelectNodes("//cite");
+
+      if (links.Count > 0)
+      {
+        foreach (var node in links)
+        {
+          var endUrl = "http://" + node.InnerHtml;
+          tempUrls.Add(endUrl);
+        }
+      }
+      System.Console.WriteLine(tempUrls.Count);
+      return tempUrls;
+    }
+    catch (NullReferenceException)
+    {
+      System.Console.WriteLine("No results were found. Be aware that some terms are blocked by the Web Engine itself.");
+      return default;
+    }
+    catch (Exception e)
+    {
+      System.Console.WriteLine(e);
+      return default;
+    }
+  }
+}
