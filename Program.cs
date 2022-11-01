075df0e810df718beff1fce6e546e8fa41b35e52
Volk <ra.sendtko@gmail.com>
2022-11-01 13:53:25 -0300

    Refactor

--- Program.cs
+++ Program.cs
@@ -1,36 +1,30 @@
 ﻿using Spectre.Console;
-
 using static Spectre.Console.AnsiConsole;
 using static OnionFinder.Services.TorService;
 using static OnionFinder.Helpers.ConsoleHelper;
 using static OnionFinder.Helpers.Messages;
 
-public class Program
-{
-    public static async Task Main()
-    {
-        while (true)
-        {
-            await LoadTor();
-            
-            WelcomeMsg();
 
-            while (true)
-            {
-                try
-                {
-                    var keyword = Ask<string>("[bold] Pesquisar:[/]").Replace(" ", "+").EscapeMarkup();
-                    
-                    await RunSearch(keyword);
-                    
-                    endOfResultsRule();
-                }
-                catch (TaskCanceledException) {}
-                catch (HttpRequestException)
-                {
-                    break;
-                }
-            }
+while (true)
+{
+    await LoadTor();
+
+    WelcomeMsg();
+
+    while (true)
+    {
+        try
+        {
+            var keyword = Ask<string>("[bold] Pesquisar:[/]").Replace(" ", "+").EscapeMarkup();
+
+            await RunSearch(keyword);
+
+            endOfResultsRule();
+        }
+        catch (TaskCanceledException) { }
+        catch (HttpRequestException)
+        {
+            break;
         }
     }
 }
