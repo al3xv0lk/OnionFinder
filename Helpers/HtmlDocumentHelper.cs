04bb1592f77d65e2889108a67e0bf81d44590ce4
Volk <ra.sendtko@gmail.com>
2022-11-07 12:02:10 -0300

    Refactor

--- Helpers/HtmlDocumentHelper.cs
+++ Helpers/HtmlDocumentHelper.cs
@@ -4,33 +4,33 @@ namespace OnionFinder.Helpers;
 
 public static class HtmlDocumentHelper
 {
-  public static List<string> FromAhmia(this HtmlDocument htmlDoc)
-  {
-    try
+    public static List<string> FromAhmia(this HtmlDocument htmlDoc)
     {
-      var tempUrls = new List<string>();
-      var links = htmlDoc.DocumentNode.SelectNodes("//cite");
-
-      if (links.Count > 0)
-      {
-        foreach (var node in links)
+        try
         {
-          var endUrl = "http://" + node.InnerHtml;
-          tempUrls.Add(endUrl);
+            var tempUrls = new List<string>();
+            var links = htmlDoc.DocumentNode.SelectNodes("//cite");
+
+            if (links.Count > 0)
+            {
+                foreach (var node in links)
+                {
+                    var endUrl = "http://" + node.InnerHtml;
+                    tempUrls.Add(endUrl);
+                }
+            }
+            System.Console.WriteLine(tempUrls.Count);
+            return tempUrls;
         }
-      }
-      System.Console.WriteLine(tempUrls.Count);
-      return tempUrls;
+        catch (NullReferenceException)
+        {
+            System.Console.WriteLine("No results were found. Be aware that some terms are blocked by the Web Engine itself.");
+            return default;
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine(e);
+            return default;
+        }
     }
-    catch (NullReferenceException)
-    {
-      System.Console.WriteLine("No results were found. Be aware that some terms are blocked by the Web Engine itself.");
-      return default;
-    }
-    catch (Exception e)
-    {
-      System.Console.WriteLine(e);
-      return default;
-    }
-  }
 }
