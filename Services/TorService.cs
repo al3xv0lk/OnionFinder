903a955043f0a93a6dba058b2ec9a946e4075fc7
Volk <ra.sendtko@gmail.com>
2022-11-07 10:59:39 -0300

    Refactor

--- Services/TorService.cs
+++ Services/TorService.cs
@@ -11,158 +11,157 @@ namespace OnionFinder.Services;
 
 public static class TorService
 {
-  private static string webEngine = "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/search/?q=";
+    private static string webEngine = "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/search/?q=";
 
-  private static List<string> tempUrls = new();
-  private static List<string> sitesOnline = new();
+    private static List<string> tempUrls = new();
+    static List<string> sitesOnline = new();
 
-  private static string torRoot = Path.Combine(System.AppContext.BaseDirectory, "tor");
-  private static string baseTorPath = Path.Combine(torRoot, "tor-browser_en-US", "Browser");
-  private static string torPath = Path.Combine(baseTorPath, "start-tor-browser");
-  private static string profilePath = Path.Combine(baseTorPath, "TorBrowser", "Data", "Browser", "profile.default");
-  private static HttpClient _httpClient = new HttpClient(new SocketsHttpHandler()
-  {
-    Proxy = new System.Net.WebProxy("socks5://127.0.0.1:9050"),
-    UseProxy = false
-  });
-
-
-  public static async Task LoadTor()
-  {
-    if (TestTorProccess())
+    private static string torRoot = Path.Combine(System.AppContext.BaseDirectory, "tor");
+    private static string baseTorPath = Path.Combine(torRoot, "tor-browser_en-US", "Browser");
+    private static string torPath = Path.Combine(baseTorPath, "start-tor-browser");
+    private static string profilePath = Path.Combine(baseTorPath, "TorBrowser", "Data", "Browser", "profile.default");
+    private static HttpClient _httpClient = new HttpClient(new SocketsHttpHandler()
     {
-      MarkupLine("Tor Browser iniciado :check_mark:");
-      _httpClient = new HttpClient(new SocketsHttpHandler()
-      {
         Proxy = new System.Net.WebProxy("socks5://127.0.0.1:9050"),
-        UseProxy = true
-      });
-      await TestProxy();
-    };
-  }
-
-  public static async Task RunSearch(string keyword)
-  {
-    await AnsiStatusAsync("Buscando links...", async ctx =>
-    {
-      var resultPage = await _httpClient.LoadHtmlDocument(webEngine + keyword);
-      tempUrls = resultPage.FromAhmia();
+        UseProxy = false
     });
 
-    if (tempUrls.Count > 0)
+    public static async Task LoadTor()
     {
-      MarkupLine($"Links encontrados: [bold][purple]{tempUrls.Count}[/][/]");
-      await TestUrls(tempUrls);
-    }
-    else
-    {
-      System.Console.WriteLine();
-      MarkupLine($"Nenhum resultado foi encontrado.");
-    }
-    WhiteSpace();
-    var panel = new Panel(new BreakdownChart()
-        .Width(75)
-        .AddItem("Links online", sitesOnline.Count, Color.Purple)
-        .AddItem("Links encontrados", tempUrls.Count, Color.White))
-    {
-      Header = new PanelHeader("Total", Justify.Center),
-      Padding = new Padding(1),
-      Border = BoxBorder.Rounded,
-      BorderStyle = new Style(Color.Purple)
-    };
-    Write(panel);
-
-    tempUrls.Clear();
-    sitesOnline.Clear();
-  }
-
- private static bool TestTorProccess()
-  {
-    try
-    {
-      AnsiStatus("Inicie o Tor Browser...", ctx =>
-      {
-        while (Process.GetProcessesByName("tor").Length == 0)
+        if (TestTorProccess())
         {
-          Thread.Sleep(1000);
-        }
-        return Task.CompletedTask;
-      });
+            MarkupLine("Tor Browser iniciado :check_mark:");
+            _httpClient = new HttpClient(new SocketsHttpHandler()
+            {
+                Proxy = new System.Net.WebProxy("socks5://127.0.0.1:9050"),
+                UseProxy = true
+            });
+            await TestProxy();
+        };
     }
-    catch (System.Exception)
-    {
-      return false;
-    }
-    return true;
-  }
-  private static async Task<string> TestProxy()
-  {
-    var value = string.Empty;
 
-    while (true)
+    public static async Task RunSearch(string keyword)
     {
-      try
-      {
-        await AnsiStatusAsync("Testando a conexão com o proxy...", async ctx =>
+        await AnsiStatusAsync("Buscando links...", async ctx =>
         {
-          value = await _httpClient.GetStringAsync("http://duckduckgo.com");
+            var resultPage = await _httpClient.LoadHtmlDocument(webEngine + keyword);
+            tempUrls = resultPage.FromAhmia();
         });
 
-        MarkupLine(" Proxy Tor conectado :check_mark:");
-        break;
-      }
-      catch (Exception)
-      {
-        Thread.Sleep(100);
-      }
-    };
+        if (tempUrls.Count > 0)
+        {
+            MarkupLine($"Links encontrados: [bold][purple]{tempUrls.Count}[/][/]");
+            await TestUrls(tempUrls);
+        }
+        else
+        {
+            System.Console.WriteLine();
+            MarkupLine($"Nenhum resultado foi encontrado.");
+        }
+        WhiteSpace();
+        var panel = new Panel(new BreakdownChart()
+            .Width(75)
+            .AddItem("Links online", sitesOnline.Count, Color.Purple)
+            .AddItem("Links encontrados", tempUrls.Count, Color.White))
+        {
+            Header = new PanelHeader("Total", Justify.Center),
+            Padding = new Padding(1),
+            Border = BoxBorder.Rounded,
+            BorderStyle = new Style(Color.Purple)
+        };
+        Write(panel);
 
-    return value;
-  }
-
-  private static async Task TestUrls(List<string> urls)
-  {
-    try
-    {
-      WriteLine();
-
-      var rule = new Rule("[purple]Resultados[/]");
-      rule.Alignment = Justify.Left;
-
-      Write(rule);
-
-      await AnsiStatusAsync("Testando links...", ctx =>
-      {
-        var tester = new ActionBlock<string>(async url =>
-              {
-                var htmlDoc = await _httpClient.LoadHtmlDocument(url);
-
-                var title = htmlDoc.DocumentNode.SelectSingleNode("//head/title").InnerText;
-
-                title = title.Length > 50 ? title[..50] : title;
-
-                var table = new Table()
-                {
-                  Width = 80,
-                  Border = TableBorder.Rounded,
-                  BorderStyle = new Style(Color.Purple)
-                };
-
-                table.AddColumn(new TableColumn(new Markup($"[bold]{title}[/]")));
-                table.AddRow(new Markup($"[link]{url}[/]"));
-
-                Write(table);
-                sitesOnline.Add(url);
-              }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 100 });
-
-        Parallel.ForEach(urls, (url) => tester.SendAsync(url));
-
-        tester.Complete();
-        tester.Completion.Wait();
-
-        return Task.CompletedTask;
-      });
+        tempUrls.Clear();
+        sitesOnline.Clear();
     }
-    catch (System.Exception) { }
-  }
+
+    private static bool TestTorProccess()
+    {
+        try
+        {
+            AnsiStatus("Inicie o Tor Browser...", ctx =>
+            {
+                while (Process.GetProcessesByName("tor").Length == 0)
+                {
+                    Thread.Sleep(1000);
+                }
+                return Task.CompletedTask;
+            });
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
+        return true;
+    }
+    private static async Task<string> TestProxy()
+    {
+        var value = string.Empty;
+
+        while (true)
+        {
+            try
+            {
+                await AnsiStatusAsync("Testando a conexão com o proxy...", async ctx =>
+                {
+                    value = await _httpClient.GetStringAsync("http://duckduckgo.com");
+                });
+
+                MarkupLine(" Proxy Tor conectado :check_mark:");
+                break;
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(100);
+            }
+        };
+
+        return value;
+    }
+
+    private static async Task TestUrls(List<string> urls)
+    {
+        try
+        {
+            WriteLine();
+
+            var rule = new Rule("[purple]Resultados[/]");
+            rule.Alignment = Justify.Left;
+
+            Write(rule);
+
+            await AnsiStatusAsync("Testando links...", ctx =>
+            {
+                var tester = new ActionBlock<string>(async url =>
+                {
+					var htmlDoc = await _httpClient.LoadHtmlDocument(url);
+
+					var title = htmlDoc.DocumentNode.SelectSingleNode("//head/title").InnerText;
+
+					title = title.Length > 50 ? title[..50] : title;
+
+					var table = new Table()
+					{
+						Width = 80,
+						Border = TableBorder.Rounded,
+						BorderStyle = new Style(Color.Purple)
+					};
+
+					table.AddColumn(new TableColumn(new Markup($"[bold]{title}[/]")));
+					table.AddRow(new Markup($"[link]{url}[/]"));
+
+					Write(table);
+					sitesOnline.Add(url);
+				}, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 100 });
+
+                Parallel.ForEach(urls, (url) => tester.SendAsync(url));
+
+                tester.Complete();
+                tester.Completion.Wait();
+
+                return Task.CompletedTask;
+            });
+        }
+        catch (System.Exception) { }
+    }
 }
